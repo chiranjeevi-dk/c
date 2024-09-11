e3fb8e9fceef1dc3bb29ef8e37e1b517c2b4a6ec
chiranjeevi-dk <chiranjeevidk18@gmail.com>
2024-09-11 09:44:02 +0530

    changes has been made in recursion

--- recursion/recursionNum.c
+++ recursion/recursionNum.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
+
 long fun(long n)
 {
-
-printf("%d",n);
-printf(" ");
-fun(n+1);
-
+    if(n == 7) return 0;  // Base case returns 0 instead of NULL
+    printf("%ld", n);     // Use %ld for printing long
+    printf(" ");
+    return fun(n+1);      // Recursive call
 }
-long main()
+
+int main()
 {
-printf("%d",fun(5));
-return 0;
+    printf("%ld", fun(5));  // Use %ld for printing long
+
 }
