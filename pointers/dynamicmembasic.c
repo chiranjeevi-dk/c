0961f98107bbf1b6e5ee320e6e53563f6e657d6e
chiranjeevi-dk <chiranjeevidk18@gmail.com>
2024-09-11 17:51:54 +0530

    misalenious

--- pointers/dynamicmembasic.c
+++ pointers/dynamicmembasic.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+struct Point {
+    int x;
+    int y;
+};
 
 int main() {
-    // Dynamically allocate memory for an integer
-    int *ptr = (int *)malloc(sizeof(int));
-    if (ptr == NULL) {
-        printf("Memory allocation failed.\n");
-        return 1;
-    }
-
-    *ptr = 10; // Assign value 10 to the memory location pointed to by ptr
-    printf("Address stored in ptr: %p\n", (void *)ptr); // Print the address stored in ptr
-    printf("Value at memory location: %d\n", *ptr); // Print the value at the memory location
-
-    int a = *ptr; // Assign the value stored at that memory location to variable a
-    printf("Value of a: %d\n", a); // Print the value of a
-
-    // Free dynamically allocated memory
-    free(ptr);
-
+    struct Point p = {.y = 5, .x = 10};  // Designated initializer
+    printf("x: %d, y: %d\n", p.x, p.y);
     return 0;
 }
--- pointers/funcpointerbasic.c
+++ pointers/funcpointerbasic.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 int add(int x, int y){
 return x + y;
 }
